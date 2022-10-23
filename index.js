e78721f0f8ff687733b726bf1d334b592e84c98b
maxh1231 <maxhumpherys@gmail.com>
2022-10-22 21:57:28 -0600

    updated eventListener

--- index.js
+++ index.js
@@ -1,33 +1,36 @@
 let billInput = document.getElementById('bill-input');
 let tipBtn = document.querySelectorAll('.tipBtn');
+document.querySelector('#people-input').value = 1;
 
 console.log(tipBtn)
 
 tipBtn.forEach(function (item) {
     item.addEventListener('click', function () {
+
+
         if (this.getAttribute('id') === 'five-percent') {
-            console.log(true)
-            document.querySelector('#tip-amount').textContent = (billInput.value * .05).toFixed(2)
+            let tipAmt = (billInput.value * .05).toFixed(2)
+            document.querySelector('#tip-amount').textContent = tipAmt
         }
 
         if (this.getAttribute('id') === 'ten-percent') {
-            console.log(true)
-            document.querySelector('#tip-amount').textContent = (billInput.value * .10).toFixed(2)
+            let tipAmt = (billInput.value * .10).toFixed(2)
+            document.querySelector('#tip-amount').textContent = tipAmt
         }
 
         if (this.getAttribute('id') === 'fifteen-percent') {
-            console.log(true)
-            document.querySelector('#tip-amount').textContent = (billInput.value * .15).toFixed(2)
+            let tipAmt = (billInput.value * .15).toFixed(2)
+            document.querySelector('#tip-amount').textContent = tipAmt
         }
 
         if (this.getAttribute('id') === 'twentyFive-percent') {
-            console.log(true)
-            document.querySelector('#tip-amount').textContent = (billInput.value * .25).toFixed(2)
+            let tipAmt = (billInput.value * .25).toFixed(2)
+            document.querySelector('#tip-amount').textContent = tipAmt
         }
 
         if (this.getAttribute('id') === 'fifty-percent') {
-            console.log(true)
-            document.querySelector('#tip-amount').textContent = (billInput.value * .5).toFixed(2)
+            let tipAmt = (billInput.value * .5).toFixed(2)
+            document.querySelector('#tip-amount').textContent = tipAmt
         }
     })
 })
