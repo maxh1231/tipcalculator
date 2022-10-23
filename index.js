545d81c34a6dd8e46f5431e65821d347b2917508
maxh1231 <maxhumpherys@gmail.com>
2022-10-22 22:18:11 -0600

    created Active tip button class and JS to add/remove classes

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 let billInput = document.getElementById('bill-input');
-let tipBtn = document.querySelectorAll('.tipBtn');
+let tipBtn = document.querySelectorAll('.tipBtnDeactive');
 document.querySelector('#people-input').value = 1;
 
 console.log(tipBtn)
@@ -11,26 +11,46 @@ tipBtn.forEach(function (item) {
         if (this.getAttribute('id') === 'five-percent') {
             let tipAmt = (billInput.value * .05).toFixed(2)
             document.querySelector('#tip-amount').textContent = tipAmt
+            tipBtn.forEach(function (item) {
+                item.classList.remove('tipBtnActive');
+            })
+            this.classList.add('tipBtnActive');
         }
 
         if (this.getAttribute('id') === 'ten-percent') {
             let tipAmt = (billInput.value * .10).toFixed(2)
             document.querySelector('#tip-amount').textContent = tipAmt
+            tipBtn.forEach(function (item) {
+                item.classList.remove('tipBtnActive');
+            })
+            this.classList.add('tipBtnActive');
         }
 
         if (this.getAttribute('id') === 'fifteen-percent') {
             let tipAmt = (billInput.value * .15).toFixed(2)
             document.querySelector('#tip-amount').textContent = tipAmt
+            tipBtn.forEach(function (item) {
+                item.classList.remove('tipBtnActive');
+            })
+            this.classList.add('tipBtnActive');
         }
 
         if (this.getAttribute('id') === 'twentyFive-percent') {
             let tipAmt = (billInput.value * .25).toFixed(2)
             document.querySelector('#tip-amount').textContent = tipAmt
+            tipBtn.forEach(function (item) {
+                item.classList.remove('tipBtnActive');
+            })
+            this.classList.add('tipBtnActive');
         }
 
         if (this.getAttribute('id') === 'fifty-percent') {
             let tipAmt = (billInput.value * .5).toFixed(2)
             document.querySelector('#tip-amount').textContent = tipAmt
+            tipBtn.forEach(function (item) {
+                item.classList.remove('tipBtnActive');
+            })
+            this.classList.add('tipBtnActive');
         }
     })
 })
