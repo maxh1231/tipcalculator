d5b2cc222d946dfc5f4f5f54edb7465093a94139
maxh1231 <maxhumpherys@gmail.com>
2022-10-23 02:26:15 -0600

    debug

--- index.js
+++ index.js
@@ -10,7 +10,8 @@ tipBtn.forEach(function (item) {
 
         if (this.getAttribute('id') === 'five-percent') {
             let tipAmt = (billInput.value * .05).toFixed(2)
-            document.querySelector('#tip-amount').textContent = tipAmt
+            document.querySelector('#tip-amount').textContent = `$${tipAmt}`
+            document.querySelector('#per-person').textContent = `$${tipAmt / (document.querySelector('#people-input').value)}`
             tipBtn.forEach(function (item) {
                 item.classList.remove('tipBtnActive');
             })
@@ -19,38 +20,49 @@ tipBtn.forEach(function (item) {
 
         if (this.getAttribute('id') === 'ten-percent') {
             let tipAmt = (billInput.value * .10).toFixed(2)
-            document.querySelector('#tip-amount').textContent = tipAmt
+            document.querySelector('#tip-amount').textContent = `$${tipAmt}`
+            let perPerson = (tipAmt / document.querySelector('#people-input').value).toFixed(2);
+            document.querySelector('#per-person').textContent = `$${perPerson}`
+
             tipBtn.forEach(function (item) {
                 item.classList.remove('tipBtnActive');
             })
             this.classList.add('tipBtnActive');
         }
+
 
         if (this.getAttribute('id') === 'fifteen-percent') {
             let tipAmt = (billInput.value * .15).toFixed(2)
-            document.querySelector('#tip-amount').textContent = tipAmt
+            document.querySelector('#tip-amount').textContent = `$${tipAmt} `
+            document.querySelector('#per-person').textContent = `$${tipAmt / (document.querySelector('#people-input').value)} `
             tipBtn.forEach(function (item) {
                 item.classList.remove('tipBtnActive');
             })
             this.classList.add('tipBtnActive');
         }
+
 
         if (this.getAttribute('id') === 'twentyFive-percent') {
             let tipAmt = (billInput.value * .25).toFixed(2)
-            document.querySelector('#tip-amount').textContent = tipAmt
+            document.querySelector('#tip-amount').textContent = `$${tipAmt} `
+            document.querySelector('#per-person').textContent = `$${tipAmt / (document.querySelector('#people-input').value)} `
             tipBtn.forEach(function (item) {
                 item.classList.remove('tipBtnActive');
             })
             this.classList.add('tipBtnActive');
         }
 
+
         if (this.getAttribute('id') === 'fifty-percent') {
             let tipAmt = (billInput.value * .5).toFixed(2)
-            document.querySelector('#tip-amount').textContent = tipAmt
+            document.querySelector('#tip-amount').textContent = `$${tipAmt} `
+            document.querySelector('#per-person').textContent = `$${tipAmt / (document.querySelector('#people-input').value)} `
             tipBtn.forEach(function (item) {
                 item.classList.remove('tipBtnActive');
             })
             this.classList.add('tipBtnActive');
         }
+
+
     })
 })
