5686ba7a7efaef5449a002339ca52141ccec7e50
maxh1231 <maxhumpherys@gmail.com>
2022-10-22 18:48:58 -0600

    added IDs to elements and JS function to calculate 5%

---
+++ index.js
@@ -0,0 +1,13 @@
+let billInput = document.getElementById('bill-input');
+let tipBtn = document.querySelectorAll('.tipBtn');
+
+console.log(tipBtn)
+
+tipBtn.forEach(function (item) {
+    item.addEventListener('click', function () {
+        if (this.getAttribute('id') === 'five-percent') {
+            console.log(true)
+            document.querySelector('#tip-amount').textContent = (billInput.value * .05).toFixed(2)
+        }
+    })
+})
