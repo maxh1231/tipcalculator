b34082e82576c5aeb44167f2864ae15cae9a0b99
max <maxhumpherys@gmail.com>
2025-04-14 00:11:46 -0600

    full functionality with dynamic on change listeners

--- index.js
+++ index.js
@@ -1,35 +1,75 @@
 const billInput = document.getElementById('bill-amount-input');
 const numPplInput = document.getElementById('num-people-input');
-const customTipInput = document.getElementById('custom-tip-input');
+const customTipInput = document.getElementById('custom-tip-btn');
 const tipPercentBtns = document.querySelectorAll('input[type="radio"');
 const tipPerPerson = document.getElementById('tip-per-person');
 const totalPerPerson = document.getElementById('total-per-person');
+const resetBtn = document.getElementById('result-reset-btn');
 
 tipPercentBtns.forEach((btn) => {
     btn.addEventListener('click', () => {
-        if (validateInput(billInput) && validateInput(numPplInput))
-            calcTip(btn.id);
+        resetBtn.disabled = false;
+        customTipInput.value = '';
+
+        handleInteraction(btn.id, false);
     });
 });
 
+customTipInput.addEventListener('change', () => {
+    tipPercentBtns.forEach((btn) => {
+        btn.checked = false;
+    });
+    handleInteraction(customTipInput.value, true);
+});
+
+const textChange = () => {
+    const checkedBtn = [...tipPercentBtns].find((btn) => btn.checked);
+    if (checkedBtn) handleInteraction(checkedBtn.id, false);
+    else if (customTipInput.value != '')
+        handleInteraction(customTipInput.value, true);
+};
+
+billInput.addEventListener('change', textChange);
+numPplInput.addEventListener('change', textChange);
+
+const handleInteraction = (origin, evalCustom) => {
+    const isBillValid = validateInput(billInput);
+    const isNumPplValid = validateInput(numPplInput);
+    let isCustomValid = true;
+    if (evalCustom) isCustomValid = validateInput(customTipInput);
+
+    if (isBillValid && isNumPplValid && isCustomValid) calcTip(origin);
+};
+
 const validateInput = (input) => {
-    if (!input.value || input.value === '0') {
-        input.previousElementSibling.lastElementChild.style.display = 'inline';
+    if (input.id != 'custom-tip-btn' && (!input.value || input.value === '0')) {
+        toggleError(input, "Can't be zero");
         return false;
-    } else {
-        input.previousElementSibling.lastElementChild.style.display = 'none';
-        return true;
+    } else if (parseInt(input.value) < 0) {
+        toggleError(input, "Can't be negative");
+        return false;
+    } else if (isNaN(parseInt(input.value))) {
+        toggleError(input, 'Must be a number');
+        return false;
+    }
+
+    toggleError(input, null);
+    return true;
+};
+
+const toggleError = (element, message) => {
+    if (element.parentElement.className.includes('input-error-container')) {
+        element.previousElementSibling.lastElementChild.textContent = message;
     }
 };
 
 const calcTip = (tipPercent) => {
-    console.log(tipPercent);
     const bill = billInput.value;
-    const tip = (tipPercent / 100) * bill;
+    const tip = (parseFloat(tipPercent) / 100) * bill;
     const numPpl = numPplInput.value;
     tipPerPerson.textContent = (tip / numPpl).toFixed(2);
     totalPerPerson.textContent = (
-        (parseInt(bill) + parseInt(tip)) /
+        (parseFloat(bill) + parseFloat(tip)) /
         numPpl
     ).toFixed(2);
 };
